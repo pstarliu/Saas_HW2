8921bea48fd0dbb51d9a3052f7aa063566ce6a8e
Paul Liu <pstar.liu@gmail.com>
2012-03-10 00:47:38 +1300

    part1b finished

--- part1.rb
+++ part1.rb
@@ -20,7 +20,27 @@ class Numeric
   end
 end
 
+module Enumerable
+
+  #def palindrome?
+   # normalized_str = self.each |i| {
+   # normalized_str = string.gsub(/\W/, '').downcase
+   # return normalized_str == normalized_str.reverse
+  #end
+end
+
+class String
+  def palindrome?
+    normalized_str = self.gsub(/\W/, '').downcase
+    return normalized_str == normalized_str.reverse
+  end
+end
+
 p 5.dollars
 puts 5.dollars.in(:yen)
 p 1.dollar.in(:rupees)
 p 10.rupees.in(:euro)
+p "foo".palindrome?
+p "A man, a plan, a canal -- panama".palindrome?
+p "Madam, I'm Adam!".palindrome?
+#[1, 2, 3, 2, 1].palindrome?
